ba595275ca88d3b94a4372bc9aab9cfc97db0209
calvinhus <gjpcalvinho@gmail.com>
2020-05-14 17:49:27 +0100

    Changes in style. Added grid with gauges for garden params

--- smartgarden.py
+++ smartgarden.py
@@ -130,8 +130,6 @@ def action(deviceName, action):
 
 
 if __name__ == '__main__':
-        app.static_folder = 'static'
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        app.jinja_env.cache = {}
+        app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
 
-#        app.config['SERVER_NAME'] = 'myapp.local'
-#        app.run(host=app.config['SERVER_NAME'], port=5000, debug=True)
